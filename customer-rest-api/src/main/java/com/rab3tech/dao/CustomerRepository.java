4fb4aa4c2fe6ced7444a3c360b4b8fcf7ba7f843
Sunita Bhandari <90815476+sunitab2021@users.noreply.github.com>
2022-11-02 18:09:44 -0400

    Revert "updated one"

--- customer-rest-api/src/main/java/com/rab3tech/dao/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rab3tech.dao;
-
-import com.rab3tech.dao.entity.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer> {
-
-}
